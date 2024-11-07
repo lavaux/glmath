1a7195d4f4ddc86122ad1f8ffa8a3ceadbfaf866
Guilhem Lavaux <guilhem.lavaux@iap.fr>
2024-11-07 13:56:43 +0200

    fix: add missing files for compilation

---
+++ ext_src/algo.hpp
@@ -0,0 +1,23 @@
+#ifndef __COSMOTOOL_ALGO_HPP
+#define __COSMOTOOL_ALGO_HPP
+
+namespace  CosmoTool
+{
+
+  template<typename T>
+  T cube(T a)
+  {
+    return a*a*a;
+  }
+
+  template<typename T>
+  T square(T a)
+  {
+    return a*a;
+  }
+
+
+
+}
+
+#endif
